8f61d8081d57ebc77418fe8393bf7e44babcb5f8
Alexander Kasimir <alexander@kasimir.at>
2013-04-02 21:39:12 +0200

    added apache license

--- routes/blog.js
+++ routes/blog.js
@@ -1,3 +1,19 @@
+/*
+ Copyright 2013 Alexander Kasimir
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
 
 /*
  * Blog related functions
@@ -12,7 +28,6 @@ exports.main_blog = function(req,res) {
       res.render('error',navdata);
     } else {
       navdata.blogposts=items;
-      console.log(navdata);
       res.render('blog',navdata);
     }
   });
@@ -27,7 +42,6 @@ exports.show_category = function(req, res){
       res.render('error',navdata);
     } else {
       navdata.blogposts=items;
-      console.log(navdata);
       res.render('blog',navdata);
     }
   });
@@ -41,4 +55,8 @@ exports.showfile = function(req,res) {
       gs.pipe(res);
     }
   });
+};
+
+exports.show_blogpost = function(req,res) {
+
 };
